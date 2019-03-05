f5af14870926eaf4d6face0db69edfcceedcbaeb
muammerhallac <muammerhallac@gmail.com>
2019-03-05 23:04:48 +0300

    TodoController added

---
+++ WebApplication/Controllers/TodoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        #region Fields
+        private readonly TodoDataContext _dbContext;
+        #endregion
+
+        #region Ctors
+        public TodoController(TodoDataContext context)
+        {
+            _dbContext = context;
+            if (!_dbContext.TodoItems.Any())
+            {
+                _dbContext.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _dbContext.SaveChanges();
+            }
+        }
+        #endregion
+
+        #region Apis
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoList()
+        {
+            return await _dbContext.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoById(long id)
+        {
+            var todoItem = await _dbContext.TodoItems.FindAsync(id);
+            if (todoItem == null)
+                return NotFound();
+            return todoItem;
+        }
+        #endregion
+    }
+}
---
+++ WebApplication/Data/TodoDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class TodoDataContext : DbContext
+    {
+        public TodoDataContext(DbContextOptions<TodoDataContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
